0d9fac203305e68facaef4aaedf8ee14a6dd9eae
ugochukwu <amagbaugochukwu@gmail.com>
2023-08-07 14:09:49 +0100

    updated

--- model/flightModel.js
+++ model/flightModel.js
@@ -1,40 +1,87 @@
+// const mongoose = require("mongoose");
+
+// const flightSchema = new mongoose.Schema({
+//     priceStandard: 
+//     {
+//         type:Number,
+//         required: true,
+//         // positive: true
+//     },
+//     priceFlex: 
+//     {
+//         type:Number,
+//         required: true,
+//         // positive: true
+//     },
+//     duration: 
+//     {
+//         type:String,
+//         required: true,
+//         // min: 1
+//     },
+//     airlineName: 
+//     {
+//         type:String,
+//         required: true,
+//         // min: 1
+//     },
+//     airlineLogo: 
+//     [{
+//         type:String,
+//         required: true
+//     }],
+//     public_id: [{
+//         type: String
+//     }]
+    
+// }, {timestamps: true})
+
+// const flightModel = mongoose.model("Flight", flightSchema);
+
+// module.exports = flightModel;
+
 const mongoose = require("mongoose");
 
-const flightSchema = new mongoose.Schema({
-    priceStandard: 
-    {
-        type:Number,
-        required: true,
-        // positive: true
+const flightSchema = new mongoose.Schema(
+  {
+    from: {
+      type: String,
+      required: [true, "take of city is required."],
+      lowercase: true,
     },
-    priceFlex: 
-    {
-        type:Number,
-        required: true,
-        // positive: true
+    to: {
+      type: String,
+      required: [true, "landing city is required."],
+      lowercase: true,
     },
-    duration: 
-    {
-        type:String,
-        required: true,
-        // min: 1
+    depatureTime: {
+      type: String,
+      required: [true, "depature time is required."],
+      lowercase: true,
     },
-    airlineName: 
-    {
-        type:String,
-        required: true,
-        // min: 1
+    arrivalTime: {
+      type: String,
+      required: [true, "arrival time is required."],
+      lowercase: true,
     },
-    airlineLogo: 
-    [{
-        type:String,
-        required: true
-    }],
-    public_id: [{
-        type: String
-    }]
-    
-}, {timestamps: true})
+    priceStandard: {
+      type: Number,
+      required: [true, "Standard price is required."],
+    },
+    priceFlex: {
+      type: Number,
+      required: [true, "Flex price is required."],
+    },
+    airlineName: {
+      type: String,
+      required: [true, "airline name is required."],
+    },
+    airlineLogo: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 const flightModel = mongoose.model("Flight", flightSchema);
 
--- routers/flightRouter.js
+++ routers/flightRouter.js
@@ -1,15 +1,33 @@
+// const express = require("express");
+
+
+// const {bookFlight,getAllFlights,getOneFlight,flightUpdate, deleteFlight} = require("../controller/flightController");
+// const router = require("./userRouter");
+
+// const routers = express.Router();
+
+// router.post("/flight", bookFlight);
+// router.get("/flight", getAllFlights);
+// router.get("/flight/:id", getOneFlight);
+// router.put("/flight/:id", flightUpdate);
+// router.delete("/flight/:id", deleteFlight);
+
+// module.exports = router;
+
 const express = require("express");
+const { userAuth, isAdmin } = require("../middlewares/authmiddleware");
+const {
+  createFlight,
+  findFlights,
+  findOneFlight,
+  deleteFlight,
+} = require("../controller/flightController");
 
+const router = express.Router();
 
-const {bookFlight,getAllFlights,getOneFlight,flightUpdate, deleteFlight} = require("../controller/flightController");
-const router = require("./userRouter");
-
-const routers = express.Router();
-
-router.post("/flight", bookFlight);
-router.get("/flight", getAllFlights);
-router.get("/flight/:id", getOneFlight);
-router.put("/flight/:id", flightUpdate);
-router.delete("/flight/:id", deleteFlight);
+router.post("/create-flight", userAuth, isAdmin, createFlight);
+router.get("/find-flights", findFlights);
+router.get("/flight/:flightId", findOneFlight);
+router.delete("/delete-flight/:flightId", userAuth, isAdmin, deleteFlight);
 
 module.exports = router;
